c9d08d889f0b7a84c46018bdf5ece51e645f96fb
ponmalar.pm93 <ponmalar.pm93@gmail.com>
2023-03-11 12:33:15 +0530

    practise program

---
+++ string/strmethod.js
@@ -0,0 +1,28 @@
+const string ='This is My Java  Script concepts.java script Is always Used in node js';
+const flowers1 ='lotus';
+const flowers2 ='rose';
+//const result =flowers1.charAt(2);
+//const result =flowers1.charCodeAt(2);
+//const result =flowers1.codePointAt(1);
+//const result =flowers1.concat(flowers2);
+//const result = flowers1.indexOf('t');
+//const result = flowers1.endsWith('s');
+//const result = flowers1.startsWith('lo');
+//const result =flowers1.repeat(5);
+//const result = flowers1.includes('us');
+//const result = flowers1.toUpperCase();
+//const result = flowers1.toLowerCase();
+//const result = flowers1.toLocaleUpperCase();
+//const result = flowers1.toLocaleLowerCase();
+//const result =string.slice(-10,-6);
+//const result =string.substring(9);
+//const result = string.substr(-4);
+//const result =string.replace('my','our');
+//const result = string.replaceAll('java','javas');
+//const result = string.split(' ');
+//const exp = /[A-Z]/g;
+//const result =string.match(exp);
+const result = string.search(/is/);
+
+
+console.log(result);
