799eac07a01f3884f633f8c789b99a72f12ef41f
ponmalar.pm93 <ponmalar.pm93@gmail.com>
2023-03-18 20:28:05 +0530

    index

--- app.js
+++ app.js
@@ -19,11 +19,24 @@
                       const weatherData = JSON.parse(data);
                        const temp = weatherData.main.temp
                        const description = weatherData.weather[0].description;
+                       const wind =weatherData.wind.speed;
+                       const temp_min = weatherData.main.temp_min;
+                       const temp_max =weatherData.main.temp_max;
                        const icon = weatherData.weather[0].icon;
                        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-                       res.write("<p> the weather is currently " + description + "</p>");
-                       res.write("<h1>the temperature in "+ query + " is"  + temp +  "degrees Celcius.</h1>");
-                       res.write("<img src=" + imageURL +">");
+                       //res.write("<body style='background-color: aqua;'>");
+                       res.write("<body style='background-color: white;'>");
+res.write("<div style='background-color: aqua;  position: center;width: 40%; height: 50%; border: 1px solid black;'>");
+                      //  res.write("<body style='height: 400px; width: 400px;border: 1px solid black;'>");
+                       res.write("<p> The weather is currently " + description + "</p>");
+                       res.write("<h1 style='color: blue;font-size: 18px;'>The temperature in "  + query +  " is "    + temp +  " degrees Celcius.</h1>");
+                       res.write("<img style='background-color: yellow;' src=" + imageURL +">");
+                       res.write("<p style='color: green; font-size: 18px;'>wind speed is " + wind + "</p>");
+                       //res.write("<p>wind speed is" + wind +"</p>");
+                       res.write("<p style='color: red;font-size: 18px;'>Temp_min: "+ temp_min +"</p>");
+                       res.write("<p style='color: red;font-size: 18px;'>Temp_max: " +temp_max + "</p>");
+                       res.write("</div>");
+                       res.write("</body>");
                        res.send()
                     })
                     });
